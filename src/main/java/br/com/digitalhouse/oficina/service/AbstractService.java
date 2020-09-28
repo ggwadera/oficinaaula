0612f12f8e053c4ad0338d85509e5f0694e5867d
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-09-28 00:22:15 -0300

    refatorar serviço em classe abstrata

---
+++ src/main/java/br/com/digitalhouse/oficina/service/AbstractService.java
@@ -0,0 +1,44 @@
+package br.com.digitalhouse.oficina.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.digitalhouse.oficina.model.Identifiable;
+
+public class AbstractService<T extends Identifiable<ID>, ID> {
+
+	private final JpaRepository<T, ID> repository;
+
+	public AbstractService(JpaRepository<T, ID> repository) {
+		this.repository = repository;
+	}
+
+	public T create(T object) {
+		object.setId(null);
+		return this.repository.save(object);
+	}
+
+	public T update(ID id, T object) {
+		T old = this.findById(id);
+		object.setId(old.getId());
+		return this.repository.save(object);
+	}
+
+	public T findById(ID id) {
+		Optional.ofNullable(id).orElseThrow(() -> new RuntimeException("O id não pode ser nulo"));
+		return this.repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Não foi possivel encontrar um objeto com id " + id));
+	}
+
+	public List<T> findAll() {
+		return this.repository.findAll();
+	}
+
+	public void deleteById(ID id) {
+		this.findById(id);
+		this.repository.deleteById(id);
+	}
+
+}
