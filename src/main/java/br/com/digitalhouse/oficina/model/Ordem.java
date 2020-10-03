7a5a331737ac8da61008fc64ae7016956601ccd7
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-10-02 21:59:30 -0300

    ordem: alterado relação de tabelas

--- src/main/java/br/com/digitalhouse/oficina/model/Ordem.java
+++ src/main/java/br/com/digitalhouse/oficina/model/Ordem.java
@@ -1,13 +1,18 @@
 package br.com.digitalhouse.oficina.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,22 +25,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Ordem implements Identifiable<Long> {
+public class Ordem implements Identifiable<Long>, Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue
+	@EqualsAndHashCode.Include
 	private Long id;
-	
-	@OneToOne
+
+	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
-	@OneToOne
+
+	@ManyToOne
 	@JoinColumn(name = "veiculo_id")
 	private Veiculo veiculo;
-	
+
 	private String descricao;
-	
+
 	private BigDecimal valor;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
 
 }
